65ab04d913f17990a77a168ad7ae38a094f7e0bd
DRomeroL-ES <damian.romero@ikasle.egibide.org>
2023-01-24 13:57:54 +0100

    Modifying models and inheritations

--- app/Models/Alumno.php
+++ app/Models/Alumno.php
@@ -5,7 +5,13 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Alumno extends Model
+class Alumno extends Persona
 {
     use HasFactory;
+    protected table = 'alumnos';
+    // Inherit all attributes from Persona
+    protected $fillable = [
+        'id',
+    ];
+
 }
--- app/Models/Calificacion.php
+++ app/Models/Calificacion.php
@@ -5,7 +5,17 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Calificacion extends Model
-{
+class Calificacion extends Model {
     use HasFactory;
+
+    protected $table = 'calificaciones';
+    protected $fillable = [
+        'id_usuario',
+        'id_producto',
+        'calificacion',
+        'comentario',
+        'created_at',
+        'updated_at'
+    ];
+
 }
