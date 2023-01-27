e2e9af4a1cc1268d83cc76d04d0f73dad64ea4eb
DRomeroL-ES <damian.romero@ikasle.egibide.org>
2023-01-27 14:03:17 +0100

    Layouts

---
+++ resources/views/facilitador_centro/alumnospreview.blade.php
@@ -0,0 +1,62 @@
+<div class="container">
+    <div class="card">
+        <div class="card-header d-flex align-items-center justify-content-between">
+            <h5 class="my-auto text-primary">Mis alumnos</h5>
+            <div class="input-group ml-auto w-auto">
+                <div class="form-outline">
+                    <input type="search" id="form1" class="form-control" placeholder="Buscar"/>
+                </div>
+                <button type="button" class="btn btn-primary">
+                    <i class="fas fa-search"></i>
+                </button>
+            </div>
+        </div>
+
+        <div class="card-body bg-light overflow-hidden">
+            <table class="table table-responsive">
+                <thead class="text-primary">
+                <tr>
+                    <th scope="col" class="border-dark">DNI</th>
+                    <th scope="col" class="border-dark">Nombre</th>
+                    <th scope="col" class="border-dark">Correo</th>
+                    <th scope="col" class="border-dark">Grado</th>
+                    <th scope="col" class="border-dark">Dual</th>
+                </tr>
+                </thead>
+                <tbody>
+                <tr>
+                    <th scope="row">05947386F</th>
+                    <td>Manolo del Bombo</td>
+                    <td>manolo.bombo@deusto.com</td>
+                    <td>3º SKERE</td>
+                    <td>No</td>
+                </tr>
+                <tr>
+                    <th scope="row">05947386F</th>
+                    <td>Manolo del Bombo</td>
+                    <td>manolo.bombo@deusto.com</td>
+                    <td>3º SKERE</td>
+                    <td>No</td>
+                </tr>
+                <tr>
+                    <th scope="row">05947386F</th>
+                    <td>Manolo del Bombo</td>
+                    <td>manolo.bombo@deusto.com</td>
+                    <td>3º SKERE</td>
+                    <td>No</td>
+                </tr>
+                </tbody>
+            </table>
+        </div>
+
+        <div class="card-footer">
+            <nav aria-label="Page navigation example">
+                <ul class="pagination justify-content-end my-auto">
+                    <li class="page-item">
+                        <a class="page-link link-primary" href="#">Todos los alumnos</a>
+                    </li>
+                </ul>
+            </nav>
+        </div>
+    </div>
+</div>
---
+++ resources/views/layouts/alumnostable.blade.php
@@ -0,0 +1,78 @@
+<div class="container">
+    <div class="card">
+        <div class="card-header d-flex align-items-center justify-content-between">
+            <h5 class="my-auto text-primary">Lista de alumnos</h5>
+            <div class="input-group ml-auto w-auto">
+                <div class="form-outline">
+                    <input type="search" id="form1" class="form-control" placeholder="Buscar"/>
+                </div>
+                <button type="button" class="btn btn-primary">
+                    <i class="fas fa-search"></i>
+                </button>
+            </div>
+        </div>
+
+        <div class="card-body bg-light overflow-hidden">
+            <table class="table table-responsive">
+                <thead class="text-primary">
+                <tr>
+                    <th scope="col" class="border-dark">DNI</th>
+                    <th scope="col" class="border-dark">Nombre</th>
+                    <th scope="col" class="border-dark">Correo</th>
+                    <th scope="col" class="border-dark">Grado</th>
+                    <th scope="col" class="border-dark">Dual</th>
+                    <th scope="col" class="border-dark">Tutor</th>
+                </tr>
+                </thead>
+                <tbody>
+                <tr>
+                    <th scope="row">05947386F</th>
+                    <td>Manolo del Bombo</td>
+                    <td>manolo.bombo@deusto.com</td>
+                    <td>3º SKERE</td>
+                    <td>No</td>
+                    <td>Jon Vadillo</td>
+                </tr>
+                <tr>
+                    <th scope="row">05947386F</th>
+                    <td>Manolo del Bombo</td>
+                    <td>manolo.bombo@deusto.com</td>
+                    <td>3º SKERE</td>
+                    <td>No</td>
+                    <td>Jon Vadillo</td>
+                </tr>
+                <tr>
+                    <th scope="row">05947386F</th>
+                    <td>Manolo del Bombo</td>
+                    <td>manolo.bombo@deusto.com</td>
+                    <td>3º SKERE</td>
+                    <td>No</td>
+                    <td>Jon Vadillo</td>
+                </tr>
+                </tbody>
+            </table>
+        </div>
+
+        <div class="card-footer">
+            <nav aria-label="Page navigation example">
+                <ul class="pagination justify-content-end my-auto">
+                    <li class="page-item">
+                        <a class="page-link" href="#" aria-label="Previous">
+                            <span aria-hidden="true">&laquo;</span>
+                            <span class="sr-only">Previous</span>
+                        </a>
+                    </li>
+                    <li class="page-item"><a class="page-link" href="#">1</a></li>
+                    <li class="page-item"><a class="page-link" href="#">2</a></li>
+                    <li class="page-item"><a class="page-link" href="#">3</a></li>
+                    <li class="page-item">
+                        <a class="page-link" href="#" aria-label="Next">
+                            <span aria-hidden="true">&raquo;</span>
+                            <span class="sr-only">Next</span>
+                        </a>
+                    </li>
+                </ul>
+            </nav>
+        </div>
+    </div>
+</div>
