b2ed4f0e004011e68738f5eb8857dcc6a5219f1c
DRomeroL-ES <damian.romero@ikasle.egibide.org>
2023-01-30 13:35:16 +0100

    Prepare for merge

---
+++ resources/views/alumno/create.blade.php
@@ -0,0 +1,28 @@
+<div class="container d-flex align-items-center justify-content-center flex-row">
+    <div class="card col-12 col-lg-8 p-0">
+        <div class="card-header text-primary text-center fs-3">Crear alumno</div>
+        <div class="card-body d-flex">
+            <div class="col-3 d-flex align-items-center justify-content-be m-0">
+                <img class="img-fluid" src="https://img.freepik.com/free-icon/user_318-875902.jpg" alt="Foto Default">
+            </div>
+            <div class="col-9">
+                <form>
+                    <div class="mb-3">
+                        <label for="exampleInputEmail1" class="form-label">Email address</label>
+                        <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+                        <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+                    </div>
+                    <div class="mb-3">
+                        <label for="exampleInputPassword1" class="form-label">Password</label>
+                        <input type="password" class="form-control" id="exampleInputPassword1">
+                    </div>
+                    <div class="mb-3 form-check">
+                        <input type="checkbox" class="form-check-input" id="exampleCheck1">
+                        <label class="form-check-label" for="exampleCheck1">Check me out</label>
+                    </div>
+                    <button type="submit" class="btn btn-primary">Submit</button>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
