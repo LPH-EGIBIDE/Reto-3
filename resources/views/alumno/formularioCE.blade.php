38bd1749ea9e89f025cee462bbfd026bc470a750
Julen Martinez <julen.martinez@ikasle.egibide.org>
2023-01-30 19:02:24 +0100

    Formulario cuaderno de seguimiento

---
+++ resources/views/alumno/formularioCE.blade.php
@@ -0,0 +1,43 @@
+<div class="container">
+    <div class="card">
+        <div class="card-header d-flex align-items-center justify-content-between">
+            <h5 class="my-auto">Formulario cuaderno de equipo</h5> 
+        </div>
+
+        <div class="card-body bg-light">
+          
+         <form class="ceForm">
+           <div class="form-outline">
+             <label class="form-label fw-bold text-decoration-underline" for="textAreaExample1">Actividades desarrolladas</label>
+             <textarea class="form-control" id="textAreaExample1" rows="10" placeholder="Actividades realizadas en la empresa, tareas relacionadas con la asignatura dual, formaciones."></textarea>
+             
+           </div>
+        <hr>
+            <div class="form-outline">
+             <label class="form-label fw-bold text-decoration-underline" for="textAreaExample1">Reflexión sobre el aprendizaje y progreso realizado en la empresa</label>
+             <textarea class="form-control" id="textAreaExample1" rows="10" placeholder="¿Qué competencias he trabajado? ¿Qué he aprendido? ¿Cuáles son los aspectos en los que he progresado?"></textarea>
+             
+           </div>
+        <hr>
+            <div class="form-outline">
+             <label class="form-label fw-bold text-decoration-underline" for="textAreaExample1">Identificación de problemas o dificultades y acciones de mejora a poner en marcha</label>
+             <textarea class="form-control" id="textAreaExample1" rows="10"></textarea>
+             
+           </div>
+        <hr>
+            <nav aria-label="Page navigation example" class="form-footer ceFormFooter">
+                <div class="input-group ml-auto w-auto align-items-center gap-2">
+                
+                    <label class="fw-bold">Semana:</label>
+                    <select class="form-select form-select-sm" aria-label=".form-select-sm example">
+                      <option selected>Open this select menu</option>
+                      <option>Semana x</option>
+                    </select>
+                      
+            </div>
+              <button type="submit" class="btn btn-primary">Enviar</button>
+            </nav>
+          </form>
+       </div>
+    </div>
+</div>
