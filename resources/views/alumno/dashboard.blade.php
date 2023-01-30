61a8549bcef8deba49adc7fa0d668438491f7b23
DRomeroL-ES <damian.romero@ikasle.egibide.org>
2023-01-30 10:01:52 +0100

    Alumno dashboard added

--- resources/views/alumno/dashboard.blade.php
+++ resources/views/alumno/dashboard.blade.php
@@ -1,57 +1,77 @@
 <div class="container">
     <div class="card mb-3">
-        <div class="card-header">
+        <div class="card-header d-flex align-items-center gap-2">
             <img src="https://pub-static.fotor.com/assets/projects/pages/d5bdd0513a0740a8a38752dbc32586d0/fotor-03d1a91a0cec4542927f53c87e0599f6.jpg" class="rounded-circle" alt="Imagen usuario" style="height: 100px">
-            <span class="card-title text-primary px-3 ">
-                Ficha del alumno
-            </span>
+            <span class="card-title text-primary m-0 fs-2 fw-bold">Ficha del alumno</span>
         </div>
         <div class="card-body">
-            <div class="row no-gutters">
-                <div class="col-md-9 col-lg-12">
-                    <table class="table table-responsive">
-                        <thead class="text-primary">
-                        <tr>
-                            <th class="border-dark">Datos del alumno</th>
-                            <th class="border-dark">Datos de la empresa</th>
-                            <th class="border-dark">Datos del facilitador del centro</th>
-                        </tr>
-                        </thead>
-                        <tbody>
-                        <tr>
-                            <th>
-                                <ul class="list-group list-group-flush">
-                                    <li class="list-group-item">DNI: 83746209K</li>
-                                    <li class="list-group-item">Nombre: Manolo el del Bombo</li>
-                                    <li class="list-group-item">Email: peruanito19@gmail.com</li>
-                                    <li class="list-group-item">Grado: Ingeniería informática</li>
-                                    <li class="list-group-item">Año: Segundo</li>
-                                    <li class="list-group-item">Dual: No</li>
-                                </ul>
-                            </th>
-                            <th>
-                                <ul class="list-group list-group-flush">
-                                    <li class="list-group-item">DNI: 83746209K</li>
-                                    <li class="list-group-item">Nombre: Manolo el del Bombo</li>
-                                    <li class="list-group-item">Email: peruanito19@gmail.com</li>
-                                    <li class="list-group-item">Grado: Ingeniería informática</li>
-                                    <li class="list-group-item">Año: Segundo</li>
-                                    <li class="list-group-item">Dual: No</li>
-                                </ul>
-                            </th>
-                            <th>
-                                <ul class="list-group list-group-flush">
-                                    <li class="list-group-item">DNI: 83746209K</li>
-                                    <li class="list-group-item">Nombre: Manolo el del Bombo</li>
-                                    <li class="list-group-item">Email: peruanito19@gmail.com</li>
-                                    <li class="list-group-item">Grado: Ingeniería informática</li>
-                                    <li class="list-group-item">Año: Segundo</li>
-                                    <li class="list-group-item">Dual: No</li>
-                                </ul>
-                            </th>
-                        </tr>
-                        </tbody>
-                    </table>
+            <div>
+                <div class="row">
+                    <div class="col-lg-4">
+                        <table class="table table-responsive">
+                            <thead class="text-primary">
+                            <tr>
+                                <th class="border-dark">Datos del alumno</th>
+                            </tr>
+                            </thead>
+                            <tbody>
+                            <tr>
+                                <td>
+                                    <ul class="list-group list-group-flush text-break">
+                                        <li class="list-group-item">DNI: 83746209K</li>
+                                        <li class="list-group-item">Nombre: Manolo el del Bombo</li>
+                                        <li class="list-group-item">Email: peruanito19@gmail.com</li>
+                                        <li class="list-group-item">Grado: Ingeniería informática</li>
+                                        <li class="list-group-item">Año: Segundo</li>
+                                        <li class="list-group-item">Dual: No</li>
+                                    </ul>
+                                </td>
+                            </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                    <div class="col-lg-4">
+                        <table class="table table-responsive">
+                            <thead class="text-primary">
+                            <tr>
+                                <th class="border-dark">Datos de la empresa</th>
+                            </tr>
+                            </thead>
+                            <tbody>
+                            <tr>
+                                <td>
+                                    <ul class="list-group list-group-flush text-break">
+                                        <li class="list-group-item">CIF: 83746209K</li>
+                                        <li class="list-group-item">Nombre: Harbour Corporation</li>
+                                        <li class="list-group-item">Dirección: Manuel Díaz de Arcaya Kalea, s/n</li>
+                                        <li class="list-group-item">Teléfono: 642069247</li>
+                                        <li class="list-group-item">Área: Ingeniería</li>
+                                    </ul>
+                                </td>
+                            </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                    <div class="col-lg-4">
+                        <table class="table table-responsive">
+                            <thead class="text-primary">
+                            <tr>
+                                <th class="border-dark">Datos del facilitador del centro</th>
+                            </tr>
+                            </thead>
+                            <tbody>
+                            <tr>
+                                <td>
+                                    <ul class="list-group list-group-flush text-break">
+                                        <li class="list-group-item">Nombre: Alex Martínez</li>
+                                        <li class="list-group-item">Email: alex.martinez@deusto.com</li>
+                                        <li class="list-group-item">Teléfono: 642069247</li>
+                                    </ul>
+                                </td>
+                            </tr>
+                            </tbody>
+                        </table>
+                    </div>
                 </div>
             </div>
         </div>
