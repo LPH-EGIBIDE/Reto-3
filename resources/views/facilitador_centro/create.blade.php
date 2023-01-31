2316ae016d078b8ee1cc4468a150104c743c6fe9
DRomeroL-ES <damian.romero@ikasle.egibide.org>
2023-01-31 14:09:21 +0100

    Hotfixes

--- resources/views/facilitador_centro/create.blade.php
+++ resources/views/facilitador_centro/create.blade.php
@@ -1,6 +1,6 @@
 <div class="container d-flex align-items-center justify-content-center">
     <div class="card col-12 col-lg-8 p-0">
-        <div class="card-header text-primary fw-bold fs-3">Crear alumno</div>
+        <div class="card-header text-primary fw-bold fs-3">Crear facilitador del centro</div>
         <div class="card-body d-flex flex-row">
             <div class="col-3 d-flex flex-column align-items-center justify-content-be m-0 me-4 p-3 gap-3">
                 <img class="img-fluid" src="https://img.freepik.com/free-icon/user_318-875902.jpg" alt="Foto Default">
@@ -21,9 +21,16 @@
                         <label for="dni" class="form-label">DNI</label>
                         <input type="text" class="form-control" id="dni">
                     </div>
-                    <div class="mb-4">
+                    <div class="mb-3">
                         <label for="telefono" class="form-label">Teléfono</label>
                         <input type="text" class="form-control" id="telefono">
+                    </div>
+                    <div class="mb-4">
+                        <label for="empresa" class="form-label">Empresa</label>
+                        <select class="form-select" aria-label="empresa">
+                            <option>Empresa 1</option>
+                            <option>Empresa 2</option>
+                        </select>
                     </div>
                     <div class="d-flex justify-content-end w-100">
                         <button type="submit" class="btn btn-success p-2 px-3">Crear</button>
