a4365d22edf436adfbc88fd2188ca30a1bef0ca8
Julen Martinez <julen.martinez@ikasle.egibide.org>
2023-02-01 09:34:01 +0100

    lista personas

---
+++ resources/views/personas.blade.php
@@ -0,0 +1,82 @@
+<div class="container">
+    <div class="card">
+        <div class="card-header d-flex align-items-center justify-content-between">
+            <h5 class="my-auto text-primary">Lista de usuarios</h5>
+            <div class="input-group ml-auto w-auto gap-2">
+                
+              <div class="form-outline">
+                    <input type="search" id="form1" class="form-control" placeholder="Buscar"/>
+                </div>
+              
+              <select class="form-select" aria-label="Default select example">
+  <option selected>Tipo de usuario</option>
+  <option value="1">Facilitador empresa</option>
+  <option value="2">Facilitador Universidad</option>
+  <option value="3">Coordinador</option>              
+  <option value="4">Alumno</option>
+  
+</select>
+
+                <button type="button" class="btn btn-primary">
+                    <i class="fas fa-search"></i>
+                </button>
+            </div>
+        </div>
+
+        <div class="card-body bg-light overflow-hidden">
+            <table class="table table-responsive">
+                <thead class="text-primary">
+                <tr>
+                    <th scope="col" class="border-dark">DNI</th>
+                    <th scope="col" class="border-dark">Nombre</th>
+                    <th scope="col" class="border-dark">Telefono</th>
+                    <th scope="col" class="border-dark">Tipo</th>
+                    
+                </tr>
+                </thead>
+                <tbody>
+                <tr>
+                    <th scope="row">05947386F</th>
+                    <td>Manolo del Bombo</td>
+                    <td>625752027</td>
+                    <td>Bolas de mono</td>
+                </tr>
+                   <tr>
+                    <th scope="row">58008615Q</th>
+                    <td>Luis Gordo de Día</td>
+                    <td>635096885</td>
+                    <td>que-so</td>
+                </tr>
+                   <tr>
+                    <th scope="row">87659081T</th>
+                    <td>Florentina Caldito Blanco</td>
+                    <td>625752027</td>
+                    <td>enano albino</td>
+                </tr>
+                
+               
+                </tbody>
+            </table>
+        </div>
+
+        <div class="card-footer">
+            <nav aria-label="Page navigation example">
+  <ul class="pagination justify-content-end">
+    <li class="page-item">
+      <a class="page-link" href="#" aria-label="Previous">
+        <span aria-hidden="true">&laquo;</span>
+      </a>
+    </li>
+    <li class="page-item"><a class="page-link" href="#">1</a></li>
+    <li class="page-item"><a class="page-link" href="#">2</a></li>
+    <li class="page-item"><a class="page-link" href="#">3</a></li>
+    <li class="page-item">
+      <a class="page-link" href="#" aria-label="Next">
+        <span aria-hidden="true">&raquo;</span>
+      </a>
+    </li>
+  </ul>
+</nav>
+        </div>
+    </div>
+</div>
